9bb2f51defadb5088659422c9e35ef3ab48d0942
Aldric2023 <2020151626@ub.edu.bz>
2023-02-20 07:19:59 -0600

    not working

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
+// mutexes
 package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main(){
-	fmt.Println("Just cuz im bored")
+func withdraw(balance *int, wg *sync.WaitGroup, mx *sync.Mutex, amount int){
+	defer wg.Done()
+	defer mx.Unlock()
 
+	//get exclusive acceess to the thing we want to change :: balance variable
+	mx.Lock()
+
+	//critical region
+	*balance = *balance - amount;
+
+	mx.Unlock()
+}
+
+func deposit(balance *int, wg *sync.WaitGroup, mx *sync.Mutex, amount int){
+	defer wg.Done()
+	defer mx.Unlock()
+
+	//get exclusive acceess to the thing we want to change :: balance variable
+	mx.Lock()
 	
+	//critical region
+	*balance = *balance + amount;
+
+	mx.Unlock()
+	
+}
+
+func main(){
+
+	balance := 1000;
+
+	//create waitgroup
+	var wg sync.WaitGroup
+	//create mutex
+	var mx sync.Mutex
+	//create my go routines
+	wg.Add(2)
+
+	go deposit(&balance, &wg, &mx,100)
+
+	go withdraw(&balance, &wg, &mx, 40)
+
+
+	//wait on the go routines to finish
+
+	wg.Wait()
+
+	fmt.Println("Balance: ",balance)
+
 }
